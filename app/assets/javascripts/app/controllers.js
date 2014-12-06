898f0a4171c5be4b1a37d30bf425f20c3565a069
Eric Brooke <ericbrooke@mac.com>
2014-12-05 17:50:35 -0800

    wired up sevices and controllers - bugged

---
+++ app/assets/javascripts/app/controllers.js
@@ -0,0 +1,9 @@
+angular.module('myApp.controllers', [])
+.controller('HomeController',
+  function($scope, ArticleService) {
+    ArticleService.getLatestFeed()
+    .then(function(data){
+      $scope.articles = data;
+    })
+
+});
