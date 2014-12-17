7b412e548739beae3a8a7b9a1635b76b370c6dbf
Eric Brooke <ericbrooke@mac.com>
2014-12-17 13:49:11 -0800

    add CORS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,22 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
+
+  before_action :set_cors_headers
+  before_action :cors_preflight
 
   after_action  :set_csrf_cookie_for_ng
 
-  private
-
-  def set_csrf_cookie_for_ng
-    cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+  def set_cors_headers
+    headers['Access-Control-Allow-Origin'] = AppConfig.client['origin']
+    headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = '*'
+    headers['Access-Control-Max-Age'] = "3628800"
   end
 
-  def verified_request?
-    super || form_authenticity_token ==
-    request.headers['HTTP_X_XSRF_TOKEN']
+  def cors_preflight
+    head(:ok) if request.method == :options
   end
-  
+
 end
