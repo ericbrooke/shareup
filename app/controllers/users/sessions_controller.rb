8d1c846ef2ceb547087c44bf9b86843b1840de67
Eric Brooke <ericbrooke@mac.com>
2014-12-17 15:22:56 -0800

    back-end login

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,32 +1,33 @@
 class Users::SessionsController < Devise::SessionsController
+
+  skip_before_action :verify_authenticity_token
+
+  before_action :authernticate_user!, except: [:create]
   respond_to :json
 
-  def show_current_user
-    reject_if_not_authorized_request!
+  def create
+    resource = User.find_for_database_authentication(email: params[:user][:email])
+    return failure unless resource
+    return failure unless resource.valid_password?(params[:user][:password])
+
     render status: 200,
     json: {
       success: true,
-      info: "Current user",
-      user: current_user
+      info: "Logged in",
+      data: {
+        auth_token: current_user.authenticate_token
+      }
     }
   end
 
   def failure
-    render status: 401,
+    warden.custom_failure!
+    render status: 200,
     json: {
       success: false,
-      info: "Unauthorized"
+      info: "Login failed",
+      date: {}
     }
-
-  end
-
-  private
-
-  def reject_if_not_authorized_request!
-    warden.authenticate!(
-    scope: resource_name,
-    recall: "#{controller_path}#failure"
-    )
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,22 +2,28 @@ Rails.application.routes.draw do
 
 
     scope '/api' do
+
+      resources :shares
+
       devise_for :users,
       :controllers => {
         :omniauth_callbacks => "users/omniauth_callbacks",
-        registrations: "users/registrations"
+        registrations: "users/registrations",
+        sessions: "users/sessions"
         }
-      end
     end
 
+    get '/dashboard' => 'welcome#dashboard'
+    root to: 'welcome#index'
+
     #
-    # root to: 'welcome#index'
+    #
     #
     # namespace :api do
     #   resources :shares
     # end
-    
-  # get '/dashboard' => 'welcome#dashboard'
+
+
 
   # devise_scope:user do
   #   get '/api/current_user' => 'users/sessions#show_current_user', as: 'show_current_user'
