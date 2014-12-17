4e997cdef0aabe230221552cff294938ddc7d8f8
Eric Brooke <ericbrooke@mac.com>
2014-12-17 15:05:51 -0800

    finished ang  wiring for auth_token

--- client/app/scripts/controllers/login.js
+++ client/app/scripts/controllers/login.js
@@ -1,2 +1,19 @@
 angular.module('shareupApp')
-.controller('LoginCtrl', function() {});
+.controller('LoginCtrl', function($location, $scope, $http) {
+
+  $scope.signup = function(){
+    $http({
+      url: '/api/users',
+      method: 'POST',
+      data: {
+        user: $scope.user
+      }
+    }).success(function(data) {
+      tokenHandler.set( data.auth_token );
+      $location.path('/');
+    }).error(function(reason){
+      $scope.user.errors = reason;
+    });
+  };
+
+});
