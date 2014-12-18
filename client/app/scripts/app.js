dec2ddb20c58132a65c5169eb71395964a7e13d1
Eric Brooke <ericbrooke@mac.com>
2014-12-17 17:14:40 -0800

    added http interceptor

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -17,6 +17,24 @@ angular
     'ngSanitize',
     'ngTouch'
   ])
+  .config(function($httpProvider) {
+    var interceptor = ['$rootScope', '$location', '$q',
+    function($scope, $location, $q){
+      var success = function(resp) { return resp; },
+      err = function(resp) {
+        if (resp.status == 401) {
+          var d = $q.defer();
+          $scope.$broadcast('event:unauthorized');
+          return d.promise;
+        };
+        return $q.reject(resp);
+      };
+      return function(promise){
+        return promise.then(success,err);
+      }
+    }];
+    $httpProvider.responseInterceptors.push(interceptor);
+  })
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
