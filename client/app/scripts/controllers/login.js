00418e92206eb1bb97573354de4dd33349f5fa02
Eric Brooke <ericbrooke@mac.com>
2014-12-17 16:57:06 -0800

    ng login function

--- client/app/scripts/controllers/login.js
+++ client/app/scripts/controllers/login.js
@@ -1,5 +1,5 @@
 angular.module('shareupApp')
-.controller('LoginCtrl', function($location, $scope, $http) {
+.controller('LoginCtrl', function($location, $scope, $http, tokenHandler) {
 
   $scope.signup = function(){
     $http({
@@ -13,6 +13,27 @@ angular.module('shareupApp')
       $location.path('/');
     }).error(function(reason){
       $scope.user.errors = reason;
+    });
+  };
+
+  $scope.login = function() {
+    $http({
+      url: '/api/users/sign_in',
+      method: 'POST',
+      data: {
+        user: $scope.user
+      }
+    }).success(function(data) {
+      if (data.success) {
+        $scope.ngModel = data.data.data;
+        tokenHandler.set(data.data.auth_token);
+        $location.path('/');
+      } else {
+          $scope.ngModel = data;
+          $scope.user.errors = data.info;
+      }
+    }).error(function(msg) {
+      $scope.user.errors = "Something is wrong with the service";
     });
   };
 
